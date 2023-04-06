8a19cb24da6d4ee3e3500be6fb97d0c6530889d2
Vudung <nguyenvudung030121@gmail.com>
2023-04-06 10:42:53 +0700

    Async Completed

--- AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.kt
+++ AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.kt
@@ -4,7 +4,9 @@ import android.database.DatabaseUtils
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.text.PrecomputedTextCompat.Params
 import androidx.databinding.DataBindingUtil
 import androidx.loader.content.AsyncTaskLoader
 import com.example.asynctask.databinding.ActivityMainBinding
@@ -19,8 +21,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun uploadTask(view: View){
-
+        println("ONCLICK HERE")
+        UploadTask().execute()
     }
 
-    class UploadTask():AsyncTaskLoader()
+    inner class UploadTask:AsyncTask<String,Int,String>() {
+        override fun doInBackground(vararg params: String?): String {
+            Log.d("Vd","Running")
+            for (i in 0..9){
+                Thread.sleep(1000)
+                publishProgress(i);
+            }
+
+
+            return "Task completed"
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            binding.textView.text = "Uploading data"
+            binding.progressIndicator.visibility = View.VISIBLE
+            binding.button.isEnabled = false
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            binding.textView.text = result
+            binding.progressIndicator.visibility = View.INVISIBLE
+            binding.button.isEnabled = true
+
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+            binding.progressBar.progress = values[0]!!+1
+        }
+    }
+
+
 }
