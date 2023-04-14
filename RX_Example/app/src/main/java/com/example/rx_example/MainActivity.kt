2ab3f34c5ccf554e6f6a7fbc96bb103159e5a99b
Vũ Dũng <nguyenvudung030121@gmail.com>
2023-04-14 11:12:19 +0700

    Rx-Basic-Done

--- RX_Example/app/src/main/java/com/example/rx_example/MainActivity.kt
+++ RX_Example/app/src/main/java/com/example/rx_example/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.rx_example
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rx_example.ui.login.LoginActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 
 /*
-    - Khai báo 1 CompositeDisposable để hủy Observeble khi activity bị destroy
+    - Khai báo 1 CompositeDisposable.Clear() để hủy Observer khi không còn muốn lắng nghe từ Observable nữa - đặt nó ở Destroy
+    - Add đăng ký observer vào disposable bằng disposable.add
     - Tạo 1 Hàm Observable để phát ra danh sách dữ liệu
     - Tạo 1 Hàm Observer để nhận dữ liệu
     - Dùng hàm Observable để đăng ký Observer
     - Có thể sử dụng JavaxKotlin sau đó dùng to Observable để biến list thành observable
+    - Dùng Operator (toán tử) - đối tượng dùng để biến đổi dữ liệu phát ra
 */
 
 class MainActivity : AppCompatActivity() {
 
-    private val disposable = CompositeDisposable()
+    private var disposable = CompositeDisposable()
     private val TAG: String = MainActivity::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,31 +36,61 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button)
         val button2: Button = findViewById(R.id.button_2)
+        val button3: Button = findViewById(R.id.btn_act_2)
 
         button.setOnClickListener {
-            val footbalObservable = footballPlayerObservable()
 
-            val footbalObserver = footballPlayerObserver()
+/*     - subcribeOn(Schedulers.io()): Điều này bảo Observable chạy task tron background thread
+       - observeOn(AndroidSchedulers.mainThread()): Điều này báo cho Observer
+      nhận data ở Android Main Thread để bạn có thể thực hiện các hành động liên quan đến UI.
+*/
 
-            footbalObservable
-                .subscribe(footbalObserver)
+            disposable.add(footballPlayerObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter{it.startsWith("M")}
+                .subscribe({ result: String -> println("Result: $result") },  // xử lý kết quả thành công
+                    { error: Throwable -> println("Error: " + error.message) })
+            )
+
+            disposable.add(footballPlayerObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter{it.startsWith("P")}
+                .subscribe({ result: String -> println("Result: $result") },  // xử lý kết quả thành công
+                    { error: Throwable -> println("Error: " + error.message) })
+            )
+
+
 
         }
 
 
+
         button2.setOnClickListener {
             val list = listOf("1", "2", "3", "4", "5")
-//            val observable:Observable<String> = list.toObservable()
-                
+            val observable:Observable<String> = Observable.fromIterable(list)
+
+            disposable.add(observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ result: String -> println("Result: $result") },  // xử lý kết quả thành công
+                    { error: Throwable -> println("Error: " + error.message) })
+            )
+
+        }
+
+        button3.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
         }
 
 
     }
 
-    fun footballPlayerObserver(): Observer<String> {
+    fun mfootballPlayerObserver(): Observer<String> {
         return object : Observer<String> {
             override fun onSubscribe(d: Disposable) {
-                Log.d(TAG, "onSubscribe")
+                Log.d(TAG, "m - onSubscribe")
             }
 
             override fun onError(e: Throwable) {
@@ -63,7 +98,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onComplete() {
-                Log.d(TAG, "onComplete")
+                Log.d(TAG, "m - onComplete")
             }
 
             override fun onNext(t: String) {
@@ -75,13 +110,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun footballPlayerObservable(): Observable<String> {
-        return Observable.just("Ronaldo", "Messi", "Pele", "Maradona")
+        return Observable.just("Penaldo","Messi","Maradona","Pele")
     }
 
     override fun onDestroy() {
-        disposable.isDisposed
+        // Using clear will clear all, but can accept new disposable
+        disposable.clear();
+        // Using dispose will clear all and set isDisposed = true, so it will not accept any new disposable
+        disposable.dispose();
         super.onDestroy()
     }
 }
+
+
 
 
---
+++ RX_Example/app/src/main/java/com/example/rx_example/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.example.rx_example.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
