2b9c2263c9997664fbb78b93744a7561adce4d9a
Vũ Dũng <nguyenvudung030121@gmail.com>
2023-04-14 13:35:32 +0700

    MVP-Media: Controller update

--- MVP_MediaApplication/app/src/main/java/com/example/mvp_mediaapplication/song/Song_Fragment.kt
+++ MVP_MediaApplication/app/src/main/java/com/example/mvp_mediaapplication/song/Song_Fragment.kt
@@ -11,6 +11,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvp_mediaapplication.databinding.FragmentSongBinding
 import com.example.mvp_mediaapplication.main.MainActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.util.MimeTypes
 
 
 class Song_Fragment : Fragment(), SongView {
@@ -19,6 +24,9 @@ class Song_Fragment : Fragment(), SongView {
     lateinit var songAdapter: SongAdapter
     var listOfSong:MutableList<Song>? = mutableListOf()
     var mediaPlayer: MediaPlayer? = null
+
+    private var player: ExoPlayer? = null
+    private val isPlaying get() = player?.isPlaying ?: false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +38,16 @@ class Song_Fragment : Fragment(), SongView {
         songPresenter = SongPresenter(this)
         listOfSong = activity?.let { songPresenter.getMusicList(it.applicationContext) }
         songPresenter.onShowListOfSong()
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.playerView.apply {
+            showController()
+            initializePlayer()
+        }
     }
 
     override fun onShowListOfSong() {
@@ -71,6 +87,41 @@ class Song_Fragment : Fragment(), SongView {
 
     override fun onClearFilter() {
 
+    }
+
+
+    private fun initializePlayer() {
+        player = activity?.let {
+            ExoPlayer.Builder(it.applicationContext) // <- context
+                .build()
+        }
+
+        // create a media item.
+        val mediaItem = MediaItem.Builder()
+            .setUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+            .setMimeType(MimeTypes.AUDIO_UNKNOWN)
+            .build()
+
+        // Create a media source and pass the media item
+        val mediaSource = activity?.let { DefaultDataSource.Factory(it.applicationContext) }?.let {
+            ProgressiveMediaSource.Factory(
+                it // <- context
+            )
+                .createMediaSource(mediaItem)
+        }
+
+        // Finally assign this media source to the player
+        player!!.apply {
+            if (mediaSource != null) {
+                setMediaSource(mediaSource)
+            }
+            playWhenReady = false // start playing when the exoplayer has setup
+            seekTo(0, 0L) // Start from the beginning
+            prepare() // Change the state from idle.
+        }.also {
+            // Do not forget to attach the player to the view
+            binding.playerView.player = it
+        }
     }
 
 
